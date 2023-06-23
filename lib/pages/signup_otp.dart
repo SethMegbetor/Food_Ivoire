15af471431f5f6576fdb3719069471a17eab2f4f
SethMegbetor <sethmegbetor@gmail.com>
2023-06-23 18:32:27 +0000

    added sign up step 2 which is OTP

---
+++ lib/pages/signup_otp.dart
@@ -0,0 +1,349 @@
+// import 'dart:async';
+
+// import 'package:flutter/material.dart';
+
+// class SignupStepTwoPage extends StatefulWidget {
+//   final String phoneNumber;
+
+//   SignupStepTwoPage({required this.phoneNumber});
+
+//   @override
+//   _SignupStepTwoPageState createState() => _SignupStepTwoPageState();
+// }
+
+// class _SignupStepTwoPageState extends State<SignupStepTwoPage> {
+//   int countdown = 120;
+//   bool isResendActive = false;
+
+//   @override
+//   void initState() {
+//     startCountdown();
+//     super.initState();
+//   }
+
+//   void startCountdown() {
+//     setState(() {
+//       countdown = 120;
+//       isResendActive = false;
+//     });
+
+//     Timer.periodic(Duration(seconds: 1), (timer) {
+//       if (countdown > 0) {
+//         setState(() {
+//           countdown--;
+//         });
+//       } else {
+//         setState(() {
+//           isResendActive = true;
+//         });
+//         timer.cancel();
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFFFEFDF5),
+//       appBar: AppBar(
+//         title: Text('Signup Step 2'),
+//       ),
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Center(
+//                 child: Text(
+//                   'Signup Step 2',
+//                   style: TextStyle(
+//                     fontSize: 24,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 10),
+//               Center(
+//                 child: Text(
+//                   'Enter the OTP sent to:',
+//                   style: TextStyle(
+//                     fontSize: 16,
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 5),
+//               Center(
+//                 child: Text(
+//                   widget.phoneNumber,
+//                   style: TextStyle(
+//                     fontSize: 16,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               TextField(
+//                 decoration: InputDecoration(
+//                   hintText: 'OTP Confirmation',
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Perform submit action
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   primary: Colors.green,
+//                   minimumSize: Size(double.infinity, 50),
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                 ),
+//                 child: Text(
+//                   'Submit',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18,
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 10),
+//               isResendActive
+//                   ? ElevatedButton(
+//                       onPressed: () {
+//                         // Perform resend action
+//                         startCountdown();
+//                       },
+//                       style: ElevatedButton.styleFrom(
+//                         primary: Colors.grey,
+//                         minimumSize: Size(double.infinity, 50),
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(10),
+//                         ),
+//                       ),
+//                       child: Text(
+//                         'Resend',
+//                         style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: 18,
+//                         ),
+//                       ),
+//                     )
+//                   : Text(
+//                       'Resend OTP in ${countdown.toString()} seconds',
+//                       style: TextStyle(
+//                         fontSize: 16,
+//                         color: Colors.grey,
+//                       ),
+//                     ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class OTPBox extends StatelessWidget {
+  final TextEditingController controller;
+  final FocusNode focusNode;
+
+  OTPBox({required this.controller, required this.focusNode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Center(
+        child: TextField(
+          controller: controller,
+          focusNode: focusNode,
+          keyboardType: TextInputType.number,
+          textAlign: TextAlign.center,
+          maxLength: 1,
+          style: TextStyle(fontSize: 20),
+          decoration: InputDecoration(
+            counterText: '',
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignupStepTwoPage extends StatefulWidget {
+  final String phoneNumber;
+
+  SignupStepTwoPage({required this.phoneNumber});
+
+  @override
+  _SignupStepTwoPageState createState() => _SignupStepTwoPageState();
+}
+
+class _SignupStepTwoPageState extends State<SignupStepTwoPage> {
+  late List<TextEditingController> otpControllers;
+  late List<FocusNode> otpFocusNodes;
+
+  int countdown = 120;
+  bool isResendActive = false;
+
+  @override
+  void initState() {
+    otpControllers = List.generate(4, (index) => TextEditingController());
+    otpFocusNodes = List.generate(4, (index) => FocusNode());
+    startCountdown();
+    super.initState();
+  }
+
+  void startCountdown() {
+    setState(() {
+      countdown = 120;
+      isResendActive = false;
+    });
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (countdown > 0) {
+        setState(() {
+          countdown--;
+        });
+      } else {
+        setState(() {
+          isResendActive = true;
+        });
+        timer.cancel();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    otpControllers.forEach((controller) => controller.dispose());
+    otpFocusNodes.forEach((focusNode) => focusNode.dispose());
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFEFDF5),
+      appBar: AppBar(
+        title: Text('Signup Step 2'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  'Signup Step 2',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: Text(
+                  'Enter the OTP sent to:',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5),
+              Center(
+                child: Text(
+                  widget.phoneNumber,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  4,
+                  (index) => Padding(
+                    padding: EdgeInsets.only(right: 8),
+                    child: OTPBox(
+                      controller: otpControllers[index],
+                      focusNode: otpFocusNodes[index],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Perform submit action
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Submit',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              isResendActive
+                  ? ElevatedButton(
+                      onPressed: () {
+                        // Perform resend action
+                        startCountdown();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        'Resend',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    )
+                  : Text(
+                      'Resend OTP in ${countdown.toString()} seconds',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/signup_telephone.dart
+++ lib/pages/signup_telephone.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_ivoire/pages/signup_otp.dart';
 
 class SignupTelephonePage extends StatelessWidget {
   @override
@@ -8,53 +9,62 @@ class SignupTelephonePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Inscription étape 1'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Food  Ivoire',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Saisissez votre numéro de telephone',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            SizedBox(height: 24),
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.phone),
-                hintText: 'Numéro de téléphone',
-              ),
-            ),
-            SizedBox(height: 48),
-            ElevatedButton(
-              onPressed: () {
-                // Perform submit action
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green,
-                minimumSize: Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Text(
-                'Submit',
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Food  Ivoire',
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 10),
+              Text(
+                'Saisissez votre numéro de telephone',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 24),
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.phone),
+                  hintText: 'Numéro de téléphone',
+                ),
+              ),
+              SizedBox(height: 48),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignupStepTwoPage(
+                        phoneNumber: '+225 078*****07',
+                      ),
+                    ),
+                  ); // Perform submit action
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Submit',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
